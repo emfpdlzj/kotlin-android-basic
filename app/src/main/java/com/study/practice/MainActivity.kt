e37ebf9d3d0853f1d333e81b0caeb35000ec1874
emfpdlzj (PC Macbook) <redryan@ajou.ac.kr>
2025-11-05 01:50:17 +0900

    랜덤으로 생성된 변수와 타이머로 기록한 시간을 비교하여 점수를 출력하는 초 맞추기 게임 구현

--- app/src/main/java/com/study/practice/MainActivity.kt
+++ app/src/main/java/com/study/practice/MainActivity.kt
@@ -5,8 +5,9 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Timer
+import java.util.*
 import kotlin.concurrent.timer
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,21 +19,29 @@ class MainActivity : AppCompatActivity() {
 
         var isRunning = false
         var sec: Int = 0
-        val tv: TextView = findViewById(R.id.tv_hello)
-        val btn: Button = findViewById(R.id.btn_kor)
+        val tv: TextView = findViewById(R.id.tv_random)
+        val tv_t: TextView = findViewById(R.id.tv_timer)
+        val tv_p: TextView = findViewById(R.id.tv_point)
+        val btn: Button = findViewById(R.id.btn_main)
+
+            val random_box = Random()
+            val num = random_box.nextInt(1001)
+            tv.text=(num.toFloat()/100).toString()
 
         btn.setOnClickListener {
             isRunning = !isRunning
             if (isRunning == true) {
-                timerTask = timer(period = 1000) {
+                timerTask = timer(period = 10) { // 0.01초마다 sec 증가
                     sec++;
                     // UI조작을 위한 메서드
                     runOnUiThread {
-                        tv.text = sec.toString()
+                        tv_t.text = (sec.toFloat()/100).toString()
                     }
                 }
             } else {
                 timerTask?.cancel()
+                val point = abs(sec-num).toFloat()/100
+                tv_p.text=point.toString()
             }
         }
     }
