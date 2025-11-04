60b01883b6a00d5da28acbf7e44e20144fdc3e5b
emfpdlzj (PC Macbook) <redryan@ajou.ac.kr>
2025-11-05 02:23:23 +0900

    게임 참가 인원수 제한하기

--- app/src/main/java/com/study/practice/MainActivity.kt
+++ app/src/main/java/com/study/practice/MainActivity.kt
@@ -10,27 +10,31 @@ import kotlin.concurrent.timer
 import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+    var p_num = 3
+    var k = 1
+    val point_list = mutableListOf<Float>()
+    fun main(){
         setContentView(R.layout.activity_main) //xml파일 연결
 
         var timerTask: Timer? = null
 
-        var isRunning = false
+        var stage = 1
         var sec: Int = 0
-        val tv: TextView = findViewById(R.id.tv_random)
+        val tv: TextView = findViewById(R.id.tv_random2)
         val tv_t: TextView = findViewById(R.id.tv_timer)
         val tv_p: TextView = findViewById(R.id.tv_point)
+        val tv_people: TextView = findViewById(R.id.tv_people)
         val btn: Button = findViewById(R.id.btn_main)
 
-            val random_box = Random()
-            val num = random_box.nextInt(1001)
-            tv.text=(num.toFloat()/100).toString()
+        val random_box = Random() //랜덤한 숫자를 뽑을 수 있는 객체. nextInt사용.
+        val num = random_box.nextInt(1001)
+        tv.text=(num.toFloat()/100).toString()
+        btn.text= "시작"
+        tv_people.text="참가자 $k"
 
         btn.setOnClickListener {
-            isRunning = !isRunning
-            if (isRunning == true) {
+            stage++
+            if (stage == 2) {
                 timerTask = timer(period = 10) { // 0.01초마다 sec 증가
                     sec++;
                     // UI조작을 위한 메서드
@@ -38,12 +42,28 @@ class MainActivity : AppCompatActivity() {
                         tv_t.text = (sec.toFloat()/100).toString()
                     }
                 }
-            } else {
+                btn.text= "정지"
+            } else if(stage==3) {
                 timerTask?.cancel()
                 val point = abs(sec-num).toFloat()/100
+                point_list.add(point)
                 tv_p.text=point.toString()
+                btn.text="다음으로"
+                stage=0
+            }else if(stage==1){
+                if(k<p_num){k++
+                    main()
+                }else{
+                 println(point_list)
+                }
             }
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        main()
     }
 }
 
